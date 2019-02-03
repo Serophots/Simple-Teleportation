670fb716ecd2fafe342d3a35daa82805ac53b9b2
Serophots <47299955+Serophots@users.noreply.github.com>
2019-02-03 16:00:44 +0000

    Update utils.java

--- main/java/me/Serophots/SimpleTpa/utils.java
+++ main/java/me/Serophots/SimpleTpa/utils.java
@@ -23,41 +23,5 @@ public class utils {
 	      list.add(org.bukkit.ChatColor.translateAlternateColorCodes('&', string));
 	    }
 	    return list;
-	}	
-	
-	public static ItemStack createItem(Inventory inv, String material, int amount, int slot, int shortId, String displayName, String... loreString) {
-		ItemStack item;
-		List<String> lore = new ArrayList();
-		
-		item = new ItemStack(Material.getMaterial(material), amount, (short) shortId);
-		
-		ItemMeta meta = item.getItemMeta();
-		meta.setDisplayName(utils.chat(displayName));
-		
-		for(String s : loreString) {
-			lore.add(utils.chat(s));
-		}
-		
-		meta.setLore(lore);
-		item.setItemMeta(meta);
-		
-		inv.setItem(slot - 1, item);
-		
-		return item;
-	}
-	public static ItemStack createItem(Inventory inv, String material, int amount, int slot, int shortId, String displayName, List<String> loreString) {
-		ItemStack item;
-		
-		item = new ItemStack(Material.getMaterial(material), amount, (short) shortId);
-		
-		ItemMeta meta = item.getItemMeta();
-		meta.setDisplayName(utils.chat(displayName));
-		
-		meta.setLore(utils.chat(loreString));
-		item.setItemMeta(meta);
-		
-		inv.setItem(slot - 1, item);
-		
-		return item;
 	}
 }
